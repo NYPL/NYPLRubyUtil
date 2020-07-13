76194b487a1e9718d6c43e179f0344d6466ec4fb
Daniel Appel <dan.s.appel@gmail.com>
2020-07-13 16:36:46 -0400

    Begin kinesis_client

---
+++ lib/kinesis_client.rb
@@ -0,0 +1,35 @@
+require 'securerandom'
+require 'aws-sdk'
+# Model representing the result message posted to Kinesis stream about everything that has gone on here -- good, bad, or otherwise.
+
+class KinesisClient
+  attr_reader :config
+
+  def initialize(config)
+    @config = config
+  end
+
+  def <<(json_message)
+    message = Stream.encode(json_message) ## needs to be fixed
+    client = Aws::Kinesis::Client.new
+
+    resp = client.put_record({
+      stream_name: config[:stream_name],
+      data: message,
+      partition_key: SecureRandom.hex(20)
+      })
+
+      return_hash = {}
+
+      if resp.successful?
+        return_hash["code"] = "200"
+        return_hash["message"] = json_message, resp
+        $logger.info("Message sent to HoldRequestResult #{json_message}, #{resp}") if $logger
+      else
+        return_hash["code"] = "500"
+        return_hash["message"] = json_message, resp
+        $logger.error("message" => "FAILED to send message to HoldRequestResult #{json_message}, #{resp}.") if $logger
+      end
+      return_hash
+  end
+end
