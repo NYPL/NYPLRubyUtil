46dfbc0272d51613b72d08ca52f9a545bf672772
charmingduchess <86011349+charmingduchess@users.noreply.github.com>
2022-01-10 10:53:54 -0500

    WIP

--- lib/kinesis_client.rb
+++ lib/kinesis_client.rb
@@ -1,7 +1,7 @@
-require 'securerandom'
-require 'aws-sdk-kinesis'
-require_relative 'nypl_avro'
-require_relative 'errors'
+require "securerandom"
+require "aws-sdk-kinesis"
+require_relative "nypl_avro"
+require_relative "errors"
 # Model representing the result message posted to Kinesis stream about everything that has gone on here -- good, bad, or otherwise.
 
 class KinesisClient
@@ -12,17 +12,14 @@ class KinesisClient
     @stream_name = @config[:stream_name]
     @avro = nil
     @batch_size = @config[:batch_size] || 1
-    @batch = []
-    @automatically_push = @config[:automatically_push] == false ? false : true
+    @records = []
+    @automatically_push = !(@config[:automatically_push] == false)
     @client_options = config[:profile] ? { profile: config[:profile] } : {}
-    @client = Aws::Kinesis::Client.new @client_options
+    @client = Aws::Kinesis::Client.new(@client_options)
 
-    if config[:schema_string]
-      @avro = NYPLAvro.by_name(config[:schema_string])
-    end
+    @avro = NYPLAvro.by_name(config[:schema_string]) if config[:schema_string]
 
     @shovel_method = @batch_size > 1 ? :push_to_batch : :push_record
-
   end
 
   def convert_to_record(json_message)
@@ -37,7 +34,6 @@ class KinesisClient
       data: message,
       partition_key: partition_key
     }
-
   end
 
   def <<(json_message)
@@ -45,10 +41,10 @@ class KinesisClient
   end
 
   def push_record(json_message)
-    record = convert_to_record json_message
+    record = convert_to_record(json_message)
     record[:stream_name] = @stream_name
 
-    @client.put_record record
+    @client.put_record(record)
 
     return_hash = {}
 
@@ -58,20 +54,18 @@ class KinesisClient
       $logger.info("Message sent to #{config[:stream_name]} #{json_message}, #{resp}") if $logger
     else
       $logger.error("message" => "FAILED to send message to HoldRequestResult #{json_message}, #{resp}.") if $logger
-      raise NYPLError.new json_message, resp
+      raise(NYPLError.new(json_message, resp))
     end
     return_hash
   end
 
   def push_to_batch(json_message)
     begin
-      @batch << convert_to_record(json_message)
+      @records << convert_to_record(json_message)
     rescue AvroError => e
       $logger.error("message" => "Avro encoding error #{e.message} for #{json_message}")
     end
-    if @automatically_push && @batch.length >= @batch_size
-      push_records
-    end
+    push_records if @automatically_push && @records.length >= @batch_size
   end
 
   def push_batch(batch)
@@ -84,7 +78,7 @@ class KinesisClient
 
     return_message = {
       failures: resp.failed_record_count,
-      error_messages: resp.records.map {|record| record.error_message }.compact
+      error_messages: resp.records.map { |record| record.error_message }.compact
     }
 
     $logger.info("Message sent to #{config[:stream_name]} #{return_message}") if $logger
@@ -96,7 +90,21 @@ class KinesisClient
   end
 
   def push_records
-    @batch.each_slice(@batch_size) {|slice| push_batch slice}
-    @batch = []
+    if @records.length > 0 && @records.length < @batch_size
+      push_batch(@records)
+    else
+      @records.each_slice(@batch_size) { |slice| push_batch(slice) }
+    end
+    @records = []
+  end
+
+  # where should I put this?
+  def resend_failures(resp)
+    failed_records_indices = []
+    resp.records.each_with_index do |record, i|
+      failed_records_indices << i if record.error_message.length > 0
+    end
+    @records = @records.select_with_index { |_record, i| failed_records_indices.include?(i) }
+    push_records
   end
 end
